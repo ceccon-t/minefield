c0ea97dee4e061aa4d7c24ab9538ea87c46785a9
Tiago Ceccon <ceccon.t@hotmail.com>
2022-11-25 02:13:56 -0300

    Update controller to use new classes Cell and Field on some of the simpler tasks

--- minefield/src/main/java/dev/ceccon/minefield/controller/Controller.java
+++ minefield/src/main/java/dev/ceccon/minefield/controller/Controller.java
@@ -1,7 +1,10 @@
 package dev.ceccon.minefield.controller;
 
+import dev.ceccon.minefield.constants.CellState;
 import dev.ceccon.minefield.constants.Difficulty;
 import dev.ceccon.minefield.constants.PlayerAction;
+import dev.ceccon.minefield.model.Cell;
+import dev.ceccon.minefield.model.Field;
 import dev.ceccon.minefield.view.IOEngine;
 import dev.ceccon.minefield.view.IOEngineFactory;
 import dev.ceccon.minefield.view.IOEngines;
@@ -23,13 +26,30 @@ public class Controller implements PlayerActionHandler {
 
     private Difficulty currentDifficulty = Difficulty.INTERMEDIATE;
 
+    private Field field;
+
     private IOEngine ioEngine;
 
     public Controller() {
         configureForCurrentDifficulty();
         remainingFlags = totalFlags;
 
+        createField();
+
         this.ioEngine = IOEngineFactory.buildEngine(numRows, numCols, totalFlags, this, IOEngines.DEFAULT_ENGINE);
+    }
+
+    private void createField() {
+        this.field = new Field(numRows, numCols);
+        seedFieldWithMines();
+    }
+
+    private void seedFieldWithMines() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (i == j) field.setMineOn(i, j);  // Diagonal mines for debug
+            }
+        }
     }
 
     private void configureForCurrentDifficulty() {
@@ -52,31 +72,62 @@ public class Controller implements PlayerActionHandler {
         }
     }
 
+    private void updateCellDisplay(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+
+        switch (cell.getState()) {
+            case HIDDEN:
+                ioEngine.displayAsHidden(x, y);
+                break;
+            case OPEN:
+                ioEngine.displayAsOpen(x, y, cell.getAdjacentMines());
+                break;
+            case FLAGGED:
+                ioEngine.displayAsFlagged(x, y);
+        }
+    }
+
+    private void showAllMines() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                Cell cell = field.getCell(i, j);
+                if (cell.isMine()) ioEngine.displayAsMine(i, j);
+            }
+        }
+    }
+
     @Override
     public void handlePlayerActionOnCell(PlayerAction action, int x, int y) {
         if (!playing) return;
+
+        Cell cell = field.getCell(x, y);
         switch (action) {
             case ACTION_OPEN:
-                score += 10;
-                ioEngine.updateScoreDisplay(score);
-                if (x == 3 && y == 3) {
-                    playing = false;
+                if (cell.isMine()) {
+                    showAllMines();
                     ioEngine.displayDefeatMessage();
-                    return;
-                }
-                if (score > 100) {
                     playing = false;
-                    ioEngine.displayVictoryMessage();
                     return;
                 }
-                Integer randomNumberForDebug = (new Random()).nextInt(9);
-                ioEngine.displayAsOpen(x, y, randomNumberForDebug);
+                // TODO: handle checking if player won
+                // TODO: handle opening flagged cell
+                cell.setState(CellState.OPEN);
+                updateCellDisplay(field.getCell(x, y));
                 break;
             case ACTION_FLAG:
                 if (remainingFlags < 1) return;
-                remainingFlags--;
-                ioEngine.displayRemainingFlagsMessage(remainingFlags, totalFlags);
-                ioEngine.displayAsFlagged(x, y);
+                if (cell.getState().equals(CellState.FLAGGED)) {
+                    remainingFlags++;
+                    cell.setState(CellState.HIDDEN);
+                    updateCellDisplay(cell);
+                    ioEngine.displayRemainingFlagsMessage(remainingFlags, totalFlags);
+                } else if (cell.getState().equals(CellState.HIDDEN)) {
+                    remainingFlags--;
+                    cell.setState(CellState.FLAGGED);
+                    updateCellDisplay(cell);
+                    ioEngine.displayRemainingFlagsMessage(remainingFlags, totalFlags);
+                }
                 break;
         }
     }
@@ -90,6 +141,8 @@ public class Controller implements PlayerActionHandler {
         remainingFlags = totalFlags;
         score = 0;
         playing = true;
+
+        createField();
 
         ioEngine.displayRemainingFlagsMessage(remainingFlags, totalFlags);
         ioEngine.restartUI();
